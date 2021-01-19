12c9d432a572c70fd405c5adceed476992e60d27
kana <kana.kibayashi@gmail.com>
2021-01-19 18:50:22 +0900

    Add database files

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -16,7 +16,8 @@ public class IndexServlet extends HttpServlet {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
